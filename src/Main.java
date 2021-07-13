72c0435097de0cb0ee0f43abca2e4e4ef6009df6
EkaterinaMikhalko <mikhalkoea@gmail.com>
2021-07-13 20:07:15 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 2_000_000_000;
+        int receipt = 500_000_000;
+        int total = balance + receipt;
+        System.out.println(total);
+    }
+}
